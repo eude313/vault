2ae70e407e57d081b1aacb7708b2bc4baf1aed9f
eude313 <eudes.charles@student.moringaschool.com>
2021-09-04 22:11:37 +0300

    add initial changes

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('gallery/', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.viewImage, name='photo'),
     path('add/', views.addImage, name='addImage'),
